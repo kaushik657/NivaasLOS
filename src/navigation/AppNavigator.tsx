4a57401c6744674c7d8730895f241d639534edb1
Hemantsharma37 <hemant1.s@techmatrixconsulting.com>
2025-10-13 18:14:43 +0530

    added back for sf

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -7,6 +7,10 @@ import { LivelinessResult } from "../screens/LivelinessResult";
 import { FaceMatchScreen } from "../screens/FaceMatch";
 import { FaceMatchResult } from "../screens/FaceMatchResult";
 import RoutePlanScreen from "../screens/RoutePlanScreen";
+import { Alert, Linking } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { scale } from "react-native-size-matters";
+import { Colors } from "../constants/colors";
 // import { AppStackParamList } from "./types";
 
 const Stack = createStackNavigator();
@@ -22,6 +26,27 @@ export enum ScreenNames {
 }
 
 export default function AppNavigator() {
+  const openSalesforceOne = async (): Promise<void> => {
+    const leadId = "00QC400000GA6MHMA1"; // Replace with your actual Lead ID
+    const url = `salesforce1://sObject/${leadId}/view`;
+
+    try {
+      // On Android, canOpenURL will now work correctly
+      const supported = await Linking.canOpenURL(url);
+      if (supported) {
+        await Linking.openURL(url);
+      } else {
+        Alert.alert(
+          "Salesforce One is not installed",
+          "Please install Salesforce One from Play Store / App Store."
+        );
+      }
+    } catch (err) {
+      console.error(err);
+      Alert.alert("Could not open Salesforce One");
+    }
+  };
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -47,7 +72,27 @@ export default function AppNavigator() {
         name={ScreenNames.FACE_MATCH_RESULT}
         component={FaceMatchResult}
       />
-      <Stack.Screen name={ScreenNames.ROUTE_PLAN} component={RoutePlanScreen} />
+      <Stack.Screen
+        name={ScreenNames.ROUTE_PLAN}
+        component={RoutePlanScreen}
+        options={{
+          headerShown: true,
+          title: "Route Plan",
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontSize: scale(18),
+            color: "black",
+          },
+          headerLeft: () => (
+            <Ionicons
+              name="arrow-back-outline"
+              size={scale(24)}
+              style={{ marginLeft: scale(10), color: "black" }}
+              onPress={openSalesforceOne}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 }
