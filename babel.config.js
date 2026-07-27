f694580803272455e878f9066403b923334fd3be
Hemantsharma37 <hemant1.s@techmatrixconsulting.com>
2026-07-27 17:02:20 +0530

    fixed everything

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    ['@babel/plugin-transform-private-methods', {loose: true}],
-    'react-native-worklets-core/plugin',
+    [
+      '@babel/plugin-transform-typescript',
+      { isTSX: true, allowDeclareFields: true, allowNamespaces: true },
+    ],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
     'react-native-reanimated/plugin',
   ],
 };
